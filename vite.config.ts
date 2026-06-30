cae81bbbd6cb9072993fc863e55dee28f31f10f1
danielx2d4144 <1.91514444e+08+danielx2d4144@users.noreply.github.com>
2026-06-30 07:31:48 +0000

    Fix SSR build by configuring external react-router-dom/server

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,10 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      ssr: {
+        noExternal: ['react-router-dom'],
+        external: ['react-router-dom/server']
       }
     };
 });
