3a1c8768265826317901e9edb9185b07c511cb13
danielx2d4144 <1.91514444e+08+danielx2d4144@users.noreply.github.com>
2026-06-30 07:32:57 +0000

    Ensure render is async and correctly wrapped in StaticRouter

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -3,7 +3,7 @@ import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import { createRootApp } from './main';
 
-export function render(url: string) {
+export async function render(url: string) {
   const html = ReactDOMServer.renderToString(
     <React.StrictMode>
       <StaticRouter location={url}>
